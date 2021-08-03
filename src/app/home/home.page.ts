617649517fe307be71276ddf2e5762543e05f1ba
Stefani da Silva Santos <santosstefani253@gmail.com>
2021-08-02 22:28:55 -0400

    Criamos o Array de pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ // Criamos um array de pokemons
+ // [] => representa um array (Lista)
+ // {} => representa um objeto (Item)
+  listaPokemon = [
+  {
+    numero: '001',
+    nome: 'Bulbasaur',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    tipos: [
+      'Grass', 'Poison'
+    ]
+  },
+  {
+    numero: '002',
+    nome: 'Ivysaur',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+    tipos: [
+      'Grass', 'Posion'
+    ]
+  },
+  {
+    numero: '003',
+    nome: 'IVenusaur',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+    tipos: [
+      'Grass', 'Posion'
+    ]
+  },
+  {
+    numero: '004',
+    nome: 'Charmander',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+    tipos: [
+      'Fire'
+    ]
+  },
+  {
+    numero: '005',
+    nome: 'Charmeleon',
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png',
+    tipos: [
+      'Fire'
+    ]
+  },
+  ];
 
   constructor() {}
 
